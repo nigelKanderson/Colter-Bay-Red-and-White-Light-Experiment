81914fb6483ea447ac9071b79af3f756e90e28b2
nigelKanderson <nigelofanderson@gmail.com>
2026-05-22 14:59:37 -0400

    adding moon data

--- 03_extract_habitat.R
+++ 03_extract_habitat.R
@@ -24,34 +24,26 @@ add_habitat <- function(data, forest_raster, buffer = 500) {
     crs = 4326
   )
   
-  pts <- st_transform(pts, crs(forest_raster))
+  pts <- st_transform(pts, terra::crs(forest_raster))
   
-  pts$geometry <- st_buffer(pts$geometry, dist = 500)
+  pts_buf <- st_buffer(pts, dist = 500)
   
-  openness <- exact_extract(forest_raster, pts, function(values, coverage_fraction) {
+  openness <- exact_extract(forest_raster, pts_buf, function(values, coverage_fraction) {
     
     tibble(
-      forest = sum(values == 42, na.rm = TRUE),
-      open = sum(values %in% c(81,82), na.rm = TRUE),
-      developed = sum(values %in% c(21:24), na.rm = TRUE),
-      total = length(values)
-    ) %>%
-      mutate(
-        pct_forest = forest / total,
-        pct_open = open /total,
-        pct_developed = developed / total
-      )
+      pct_forest = mean(values %in% c(41,42,43), na.rm = TRUE),
+      pct_nonforest = 1 - pct_forest
+      #pct_open = mean(values %in% c(31,41,42,43,52,71,81,82), na.rm = TRUE),
+      #pct_developed = mean(values %in% c(21:24), na.rm = TRUE),
+    ) 
     
   })
   
+  openness_df <- dplyr::bind_rows(openness)
   
-  sites_env <- bind_cols(pts, openness)
-  
-  sites_env <-  pts %>%
-    sf::st_drop_geometry() %>%
-    distinct(site, .keep_all = TRUE) %>%
-    mutate(openness = openness) %>%
-    select(site, openness)
+  sites_env <- dplyr::bind_cols(
+    sites, openness_df
+  )
   
   data_out <- data %>%
     left_join(sites_env, by = "site")
@@ -61,6 +53,31 @@ add_habitat <- function(data, forest_raster, buffer = 500) {
   return(data_out)
 }
 
-#readr::write_csv(data_out, "data_out.csv")
+add_moon <- function(data) {
+  
+  library(dplyr)
+  library(suncalc)
+  
+  moon_illum <- getMoonIllumination(data$datetime)
+  
+  data %>%
+    rowwise() %>%
+    mutate(
+      moon_illum = getMoonIllumination(datetime)$fraction,
+      moon_phase = getMoonIllumination(datetime)$phase,
+      moon_altitude = getMoonPosition(
+        date = datetime,
+        lat = lat,
+        lon = lon
+      )$altitude
+    ) %>%
+    ungroup()
+  
+}
 
-#saveRDS(data_out, "data_out.rds")
+add_environmental_covariates <- function(data, forest_raster) {
+  
+  data %>%
+    add_habitat(forest_raster) %>%
+    add_moon()
+}
