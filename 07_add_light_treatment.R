291922c4346c1bf5d187e039a17d50d3385e75a1
nigelKanderson <nigelofanderson@gmail.com>
2026-06-03 11:51:30 -0400

    added light treatment file

---
+++ 07_add_light_treatment.R
@@ -0,0 +1,13 @@
+library(readr)
+library(readxl)
+library(dplyr)
+library(lubridate)
+
+lighting <- read_excel("/Users/nanderson/Library/CloudStorage/GoogleDrive-nigel_anderson@brown.edu/.shortcut-targets-by-id/1sSdpOAdUOgAVbJGpTKgB3-CJAfnjsvKJ/grandteton_distanceproject")
+
+names(lighting)
+
+lighting <- lighting %>%
+  mutate(date = as.Date(date))
+
+saveRDS(lighting, "data_full.rds")
